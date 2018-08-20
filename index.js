496cc6265fe78541045e5d42c277bfbd25abe003
sagar-pardhi <sgrpardhi007@gmail.com>
2018-08-20 22:04:49 +0530

    Added units in url variable to return proper format of data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 let apikey = 'fecdb5c442c4f003fcd4ce3e3a248953'
 let city =  'mumbai'
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`
 
 request(url, function (err, response, body) {
 	if(err) {
