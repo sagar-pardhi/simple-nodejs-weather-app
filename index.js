b757fa66b043aec10383296964d957799a4c01f7
sagar-pardhi <sgrpardhi007@gmail.com>
2018-08-20 21:52:48 +0530

    Added variable apikey, url, city

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const request = require('request');
 
+let apikey = 'fecdb5c442c4f003fcd4ce3e3a248953'
+let city =  'mumbai'
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
+
 request(url, function (err, response, body) {
 	if(err) {
 		console.log('error: ', error);
