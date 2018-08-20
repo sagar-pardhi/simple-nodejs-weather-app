509ec605a42192031bc5d82d5d4bcd50584870a5
sagar-pardhi <sgrpardhi007@gmail.com>
2018-08-20 22:09:23 +0530

    Added yargs module

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const request = require('request');
+const argv = require('yargs').argv;
 
 let apikey = 'fecdb5c442c4f003fcd4ce3e3a248953'
-let city =  'mumbai'
+let city =  argv.c || 'mumbai';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`
 
 request(url, function (err, response, body) {
