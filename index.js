14bdaceaf192273bbab8fcc7cfb678fef22d3e0d
sagar-pardhi <sgrpardhi007@gmail.com>
2018-08-20 22:02:34 +0530

    Added code to simlified jumbled data

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ request(url, function (err, response, body) {
 	if(err) {
 		console.log('error: ', error);
 	} else {
-		console.log('body: ', body);
+		let weather = JSON.parse(body)
+		let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+		console.log(message);
 	}
 });
