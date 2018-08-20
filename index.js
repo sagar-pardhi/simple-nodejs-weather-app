f7e544a0890c52c9b0eed95ba14e01e94fc80684
sagar-pardhi <sgrpardhi007@gmail.com>
2018-08-20 21:45:50 +0530

    Added starter code

---
+++ index.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+request(url, function (err, response, body) {
+	if(err) {
+		console.log('error: ', error);
+	} else {
+		console.log('body: ', body);
+	}
+});
